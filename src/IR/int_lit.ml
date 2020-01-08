83a6c927d7be8395f7b2bd20b15cd6a811b26880
Daniel Mercier <mercier@adacore.com>
2020-01-08 17:23:01 +0100

    Add of_string function in Int_lit

--- src/IR/int_lit.ml
+++ src/IR/int_lit.ml
@@ -2,6 +2,8 @@ type t = int
 
 let of_int i = i
 
+let of_string s = int_of_string s
+
 let to_int i = i
 
 let pp = Format.pp_print_int
--- src/IR/int_lit.mli
+++ src/IR/int_lit.mli
@@ -2,6 +2,8 @@ type t
 
 val of_int : int -> t
 
+val of_string : string -> t
+
 val to_int : t -> int
 
 val pp : Format.formatter -> t -> unit
